ed7a147023d7ebbec00c678717abbacd31aec3fa
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-07-02 12:31:54 +0800

    feature/add get analytic time by cctv id

--- src/masks/MaskRepository.ts
+++ src/masks/MaskRepository.ts
@@ -1,3 +1,4 @@
+import { Sequelize } from 'sequelize-typescript';
 import MaskAnalytics, { MaskAnalyticCreationAttributes } from './MaskAnalytics';
 
 const createMaskAnalyticData = async (
@@ -12,7 +13,19 @@ const getAllByCCTV = async (cctvId: number | number[]) => {
   return masks;
 };
 
+const getDailyCountByCCTV = async (cctvId: number | number[]) => {
+  const masks = await MaskAnalytics.findAll({
+    where: { cctvId: cctvId }, group: ['predictionDate', 'status'],
+    attributes: [
+      'status',
+      [Sequelize.fn('count', Sequelize.col('id')), 'count'],
+      [Sequelize.fn('date', Sequelize.col('createdAt')), 'predictionDate']]
+  })
+  return masks;
+}
+
 export default {
   createMaskAnalyticData,
   getAllByCCTV,
+  getDailyCountByCCTV
 };
