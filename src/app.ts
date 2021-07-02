908e3cc44b6b65d03599238946bbe8a40f3589f8
Putu Audi Pasuatmadi <audipasuatmadi@gmail.com>
2021-07-02 12:32:06 +0800

    feature/add count controller

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,44 @@ app.post('/', async (req, res, next) => {
   res.status(201).send('success');
 });
 
+/*
+Returns:
+[
+    {
+        "status": 1,
+        "count": 3,
+        "predictionDate": "2021-06-29"
+    },
+    {
+        "status": 3,
+        "count": 2,
+        "predictionDate": "2021-06-29"
+    },
+    ...
+]
+*/
+app.get('/count', async (req, res, next) => {
+  const { id } = req.query;
+  if (!id) return res.status(400).send('invalid request 1');
+  let finalId: number | number[] = -1;
+  if (typeof id === 'string') {
+    const filteredId = parseInt(id);
+    if (isNaN(filteredId)) return res.status(400).send('invalid request 2');
+    finalId = filteredId;
+  }
+  if (typeof id === 'object') {
+    const ids = (id as string[]).map((val) => parseInt(val));
+    const filteredIds = ids.filter((val) => !isNaN(val));
+    finalId = filteredIds;
+  }
+  try {
+    const masks = await MaskRepository.getDailyCountByCCTV(finalId);
+    res.status(200).json(masks);
+  } catch (e) {
+    next(e);
+  }
+});
+
 app.get('/:id', async (req, res, next) => {
   const { id } = req.params;
   if (!id) return res.status(400).send('invalid request');
@@ -55,5 +93,6 @@ app.get('/', async (req, res, next) => {
     next(e);
   }
 });
+
 
 export default app;
